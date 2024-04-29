5a755c98ac0f7d75e6b55ea94a340facd152844a
Neo <54811660+Neotastisch@users.noreply.github.com>
2024-04-29 17:43:17 +0200

    Update FileCache.js

--- src/common/cache/FileCache.js
+++ src/common/cache/FileCache.js
@@ -24,7 +24,6 @@ class FileCache {
             if (err) {
               reject(err);
             } else {
-              console.log("return two");
               resolve(cached);
             }
           });
